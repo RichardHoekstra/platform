9529f695f093f7ced22c123f83a4d44bc19d8ec0
Arnold Hendriks <arnold@webhare.nl>
2026-08-26 11:04:32 +0200

    [backport] stderr merge fixes, support blob output, support stdin in spawnProcessSync
    # Conflicts:
    #	whtree/modules/platform/js/harescript/process.ts

--- whtree/modules/platform/js/harescript/process.ts
+++ whtree/modules/platform/js/harescript/process.ts
@@ -1,12 +1,36 @@
-import { spawnSync } from "child_process";
+import { WebHareMemoryBlob } from "@webhare/services/src/webhareblob";
+import { spawn } from "child_process";
 
 /** The TS helper for HS spawnProcessSync, hence the name. It's async but will be made sync in HareScript */
-export async function spawnProcessSync(cmd: string, args: string[]) {
-  const proc = spawnSync(cmd, args, { encoding: "utf-8", stdio: ['ignore', 'pipe', 1] });
+export async function spawnProcessSync(cmd: string, args: string[], options?: {
+  stdin?: string;
+  output_as_blob?: boolean;
+  merge_stderr?: boolean;
+}) {
+  const stdout: Buffer[] = [], stderr: Buffer[] = [];
+  const proc = spawn(cmd, args, { stdio: ['pipe', 'pipe', 'pipe'] });
+
+  proc.stdout.on("data", chunk => stdout.push(Buffer.from(chunk)));
+  if (options?.merge_stderr)
+    proc.stderr.on("data", chunk => stdout.push(Buffer.from(chunk)));
+  else
+    proc.stderr.on("data", chunk => stderr.push(Buffer.from(chunk)));
+
+  if (options?.stdin !== undefined)
+    proc.stdin.end(options.stdin);
+  else
+    proc.stdin.end();
+
+  const result = await new Promise<{ code: number | null; signal: NodeJS.Signals | null }>((resolve, reject) => {
+    proc.on("error", reject);
+    proc.on("close", (code, signal) => resolve({ code, signal }));
+  });
+
   return {
-    error: proc.status !== 0,
+    error: result.code !== 0,
     //node 24 lacks convertProcessSignalToExitCode so we'll just map signals to -1
-    exitcode: proc.status ?? -1,
-    output: proc.stdout
+    exitcode: result.code ?? -1,
+    output: options?.output_as_blob ? WebHareMemoryBlob.from(Buffer.concat(stdout)) : Buffer.concat(stdout).toString("utf-8"),
+    stderr: Buffer.concat(stderr).toString("utf-8"),
   };
 }
